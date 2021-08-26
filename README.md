0f898ba2eb10f176505a9b3629dd2274d522119a
Xavier Serrano <xavierserranoa@gmail.com>
2021-08-26 13:04:15 -0400

    adding pogo news command  and increasing version to 1.1.0

--- README.md
+++ README.md
@@ -11,20 +11,39 @@ I have it set up on my site and nightbot like this you could call it from my sit
 
 ## Example
 
+### Raidbosses command endpoint
+
 the endpoint is `https://pnx.world/wp-json/twitchbots/v1/current-raid-boss/?tier=tier1` replace the domain if you are self-hosting it. 
 
-### Nightbot Command
+### Nightbot raidbosses Command
 
 You can add a command like `!raidbosses` and then set the message to.
 ```
-$(urlfetch https://pnx.world/wp-json/twitchbots/v1/current-raid-boss/?tier=$(1))
+$(urlfetch https://pnx.world/wp-json/twitchbots/v1/current-raid-boss/?tier=$(eval if("$(1)" == 'null' ){ "tier1"; }else{ $(1); }))
 ```
 
 Command usage: `!raidbosses tier1` or `!raidbosses tier3` or `!raidbosses tier5` or `!raidbosses mega`. the endpoint will default to tier1 if the wrong argument is passed.
 
 ### Screenshots
 
-![Alt text](/screenshots/raidbosses-command-example.png?raw=true "Raid Bosses Command Twitch Example")
+![Raid Bosses Command Twitch Example](/screenshots/raidbosses-command-example.png?raw=true "Raid Bosses Command Twitch Example")
+
+### Pogonews command endpoint
+
+the endpoint is `https://pnx.world/wp-json/twitchbots/v1/current-raid-boss/?tier=tier1` replace the domain if you are self-hosting it. 
+
+### Nightbot Pogonews Command
+
+You can add a command like `!pogonews` and then set the message to.
+```
+$(urlfetch https://pnx.world/wp-json/twitchbots/v1/latest-pogo-news/?top=$(eval if("$(1)" == 'null' ){ "1"; }else{ $(1); }))
+```
+
+Command usage: `!pogonews 1` or `!pogonews top1` or `!pogonews 2` or `!pogonews top2`. the endpoint will default to top 1 if the wrong/empty argument is passed. It limits at 2 do to character limit, there is the possibility of maybe using a url shortener to allow for more but not sure there is much usage in having really more than the top1.
+
+### Screenshots
+
+![Pogonews Command Twitch Example](/screenshots/pogonews-command-example.png?raw=true "Pogonews Command Twitch Example")
 
 Im thiking about other endpoints to add here that will be useful for my streams and possibly others.
 
--- twitchbot-endpoints.php
+++ twitchbot-endpoints.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Twitchbot Endpoints
- * Version: 1.0.0
+ * Version: 1.1.0
  */
 
 namespace Twichbot_Endpoints;
@@ -40,6 +40,10 @@ add_action( 'rest_api_init', function () {
 		'methods' => 'GET',
 		'callback' => __NAMESPACE__ . '\current_raid_boss',
 	) );
+	register_rest_route( 'twitchbots/v1', '/latest-pogo-news/', array(
+		'methods' => 'GET',
+		'callback' => __NAMESPACE__ . '\latest_pogo_news',
+	) );
 } );
 
 /**
@@ -47,7 +51,7 @@ add_action( 'rest_api_init', function () {
  */
 function current_raid_boss( \WP_REST_Request $request ) {
 	header("Content-Type: text/plain");
-	$tier = $request['tier'];
+	$tier = ( !empty( $request['tier'] ) ) ? $request['tier'] : 'tier1';
 
 	// Get any existing copy of our transient data
 	if ( false === ( $current_raid_bosses_{$tier} = get_transient( "current_raid_bosses_$tier" ) ) ) {
@@ -102,3 +106,44 @@ function current_raid_boss( \WP_REST_Request $request ) {
 	}
 }
 
+/**
+ * Current Raid Bosses endpoint callback.
+ */
+function latest_pogo_news( \WP_REST_Request $request ) {
+	header("Content-Type: text/plain");
+	$pogo_url = 'https://pokemongolive.com';
+	$top = ( !empty( $request['top'] ) ) ? absint( str_replace( array( 'top', 'Top', 'TOP' ), '' , $request['top'] ) ) : 1;
+	
+	if ( $top > 2 ) {
+		echo "WOW! slow down you can only request up to top 2 latest news.";
+	}
+	
+	// Get any existing copy of our transient data
+	if ( false === ( $latest_pogo_news_top_{$top} = get_transient( "latest_pogo_news_top_$top" ) ) ) {
+    	// It wasn't there, so regenerate the data and save the transient
+		$dom = new \DomDocument();
+		libxml_use_internal_errors(true);
+		$dom->loadHTMLFile("$pogo_url/post/?hl=en");
+		$xpath = new \DOMXpath($dom);
+
+		$elements = $xpath->query("//div[contains(@class,'post-list__title')]//a");
+
+		if ( !is_null($elements) ) {
+			for( $i=0; $i < $top; $i++ ) {
+				if ( ! is_node_empty( $elements->item($i)->nodeValue ) ) {
+					$href = $pogo_url . $elements->item($i)->getAttribute('href');
+					$output = $elements->item($i)->nodeValue . TWITCH_SUBELEMENT . $href;
+					if ( $top - 1 !== $i ) {
+						$output .= TWITCH_SPACER;
+					}
+					set_transient( "latest_pogo_news_top_$top", $output, 2 * HOUR_IN_SECONDS );
+					echo $output;
+				}
+				
+			}
+		}
+	} else {
+		// Use the data like you would have normally...
+		echo $latest_pogo_news_top_{$top};
+	}
+}
